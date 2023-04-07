5ee27f33982d3d074cbc2a446248956afc7a7313
celvineadiputradev <celvineadiputra2@gmail.com>
2023-04-07 09:57:37 +0700

    finish tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+// Add this line
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:localization_tutorial_flutter/pages/home_example.dart';
+// Add this line
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const LocalizationApp());
@@ -10,7 +15,20 @@ class LocalizationApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Localization Example',
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
+      supportedLocales: const [
+        Locale('en'),
+        Locale('id'),
+      ],
+      // locale: const Locale('en'),
       theme: ThemeData(useMaterial3: true),
+      home: const HomeExample(),
     );
   }
 }
